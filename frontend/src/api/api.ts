525e068c0b0392ce4ea949d36fb1318165bbca55
Jeshurun Ezeobi <jeshurunezeobi@s-MacBook-Pro.local>
2025-04-22 18:50:51 +0300

    configure production and local url

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
