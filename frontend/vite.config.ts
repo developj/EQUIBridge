4579c6362a4b16a3c56ef9bb868c7902b2885996
Jeshurun Ezeobi <jeshurunezeobi@s-MacBook-Pro.local>
2025-05-07 14:04:30 +0300

    host frontend on netlify

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ['equibridge.onrender.com','equibridgeapi.onrender.com','equibridge-2.onrender.com'],
+    allowedHosts: ['equibridge.onrender.com','equibridgeapi.onrender.com','equibridge-2.onrender.com,creative-cajeta-95b188.netlify.app'],
   },
   plugins: [react()],
 });
